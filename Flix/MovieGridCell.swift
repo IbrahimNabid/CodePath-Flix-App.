530520bba5c051a58d4fb507805565ac6027b62a
IbrahimNabid <79735610+IbrahimNabid@users.noreply.github.com>
2021-12-04 02:54:14 -0500

    Added user stories and tab bar options

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  MovieGridCell
+//
+//  Created by Ibrahim Nabid on 12/4/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -51,7 +51,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                  
                     self.movies = dataDictionary["results"] as! [[String:Any]]
                     self.tableview.reloadData()
-                    print(dataDictionary)
 
                     // TODO: Get the array of movies
                     // TODO: Store the movies in a property to use elsewhere
@@ -65,14 +64,27 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
 
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        print("Loading up the details screen")
+        
+        // Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableview.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        // Pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableview.deselectRow(at: indexPath, animated: true)
+        
+        
     }
-    */
+    
 
 }
